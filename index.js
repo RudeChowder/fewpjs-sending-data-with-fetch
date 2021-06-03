cdf6b24c18835434a596ed3085ff50344f320bb3
Stephen Crane <scrane88@gmail.com>
2021-06-03 10:50:12 -0400

    posted

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+const submitData = (userName, userEmail) => {
+    const url = "http://localhost:3000/users"
+    const htmlBody = document.querySelector('body')
+    
+    let data = {
+        name: userName,
+        email: userEmail
+    }
+
+    let configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+    }
+
+    return fetch(url, configurationObject)
+    .then(resp => resp.json())
+    .then(json => {
+        htmlBody.append(json.id)
+    })
+    .catch( error => {
+        htmlBody.append(error.message)
+    })
+}
